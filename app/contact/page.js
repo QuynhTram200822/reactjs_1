2dd4c450fa39195270f537c9c146e47400f249dc
QuynhTram200822 <ttqtram.20it3@vku.udn.vn>
2024-10-22 09:21:30 +0700

    first commit

---
+++ app/contact/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
+export default function Contact() {
+  return (
+    <div className={`${styles.container} `}>
+      <h3 className={`${styles.container__title}`}>Contact with me!</h3>
+    </div>
+  );
+}
